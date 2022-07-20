af8f5f94bd11e4f661727e8d28f3178d1e3caf06
erin-maxson <erin@cohatch.com>
2022-07-20 15:31:28 -0700

    activity updates

--- 04-Stu_API-HTML-Routes/Unsolved/server.js
+++ 04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -1,11 +1,26 @@
 // TODO: Import express
-
-// TODO: Import 'terms.json' file
+const express = require('express');
+// TODO: Import 'terms.json' fileconst PORT = 3001;
+const app = express();
+const path = require("./terms.json")
 const PORT = 3001;
 
 // TODO: Initialize app variable
 
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
+app.get('/', (req, res) => {
+  res.send(
+    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
+  );
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    term: 'api',
+    description:
+      'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
